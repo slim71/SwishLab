d038b75d1be8a0172d37abd3c2a2a4cf92ce9999
Simone Vollaro <39267642+slim71@users.noreply.github.com>
2026-01-16 21:45:34 +0100

    Loading page

---
+++ lib/pages/loading_page.dart
@@ -0,0 +1,195 @@
+import 'package:SwishLab/models/analysis_state.dart';
+import 'package:SwishLab/providers/shooting_analysis_provider.dart';
+import 'package:SwishLab/styles/colors.dart';
+import 'package:SwishLab/styles/styles.dart';
+import 'package:SwishLab/widgets/transparent_button.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import 'package:lottie/lottie.dart';
+
+class LoadingPage extends ConsumerStatefulWidget {
+  const LoadingPage({super.key});
+
+  @override
+  ConsumerState<LoadingPage> createState() => _LoadingPageWidgetState();
+}
+
+class _LoadingPageWidgetState extends ConsumerState<LoadingPage> {
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final appColors = Theme.of(context).extension<AppColorSet>()!;
+
+    ref.listen<AnalysisState>(
+      shootingAnalysisProvider,
+      (previous, next) {
+        if (next is AnalysisSuccess) {
+          context.goNamed('results');
+        }
+
+        if (next is AnalysisFailure) {
+          showDialog(
+            context: context,
+            builder: (_) => AlertDialog(
+              title: const Text('Analysis failed'),
+              content: const Text('Error! Navigate home?'),
+              actions: [
+                TextButton(
+                  onPressed: () => context.pop(),
+                  child: const Text('Stay'),
+                ),
+                TextButton(
+                  onPressed: () {
+                    context.pop();
+                    context.goNamed('home');
+                  },
+                  child: const Text('Go home'),
+                ),
+              ],
+            ),
+          );
+        }
+      },
+    );
+
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+        FocusManager.instance.primaryFocus?.unfocus();
+      },
+      child: Scaffold(
+        key: scaffoldKey,
+        backgroundColor: appColors.primaryBackground,
+        body: SafeArea(
+          top: true,
+          child:
+              // Container with the content for the loading page
+              Semantics(
+            label: 'Main container content',
+            child: Container(
+              width: double.infinity,
+              height: double.infinity,
+              decoration: BoxDecoration(
+                gradient: appColors.gradientBackground(),
+              ),
+              child:
+                  // Column to place the content for the loading page
+                  Padding(
+                padding: EdgeInsets.all(16),
+                child: Semantics(
+                  label: 'Main column content',
+                  child: Column(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      // Loading animation
+                      Semantics(
+                        label: 'Loading animation',
+                        child: Lottie.asset(
+                          'assets/jsons/Loader_basketball.json',
+                          width: 400,
+                          height: 400,
+                          fit: BoxFit.contain,
+                          animate: true,
+                        ),
+                      ),
+
+                      // "Processing Video" text
+                      Padding(
+                        padding: EdgeInsetsDirectional.fromSTEB(32, 16, 32, 0),
+                        child: Semantics(
+                          label: '"Processing Video" text',
+                          child: Text(
+                            'Processing Video',
+                            style: AppTextStyles.headlineLarge(context),
+                          ),
+                        ),
+                      ),
+
+                      // Text to ask the user to wait a bit
+                      Padding(
+                        padding: EdgeInsetsDirectional.fromSTEB(32, 8, 32, 0),
+                        child: Semantics(
+                          label: '"Please wait" text',
+                          child: Text(
+                            'Please wait while we prepare your video',
+                            textAlign: TextAlign.center,
+                            style: AppTextStyles.labelLarge(context),
+                          ),
+                        ),
+                      ),
+
+                      // Container used to place a custom divider
+                      Padding(
+                        padding: EdgeInsetsDirectional.fromSTEB(32, 32, 32, 0),
+                        child: Semantics(
+                          label: 'Container used to place a custom divider',
+                          child: Container(
+                            width: 240,
+                            height: 8,
+                            decoration: BoxDecoration(
+                              color: appColors.alternateOne,
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                            child:
+                                // Container used as colored divider
+                                Semantics(
+                              label: 'Container used as colored divider',
+                              child: Container(
+                                width: 120,
+                                height: 8,
+                                decoration: BoxDecoration(
+                                  gradient: appColors.gradientLinear(),
+                                  borderRadius: BorderRadius.circular(12),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+
+                      // Text stating that the loading might take a while
+                      Padding(
+                        padding: EdgeInsetsDirectional.fromSTEB(32, 24, 32, 0),
+                        child: Semantics(
+                          label: '"Might take a while" text',
+                          child: Text(
+                            'This may take a moment depending on the video size',
+                            textAlign: TextAlign.center,
+                            style: AppTextStyles.labelLarge(context),
+                          ),
+                        ),
+                      ),
+
+                      // Back button to stop waiting and discard results
+                      Padding(
+                        padding: EdgeInsetsDirectional.fromSTEB(0, 48, 0, 0),
+                        child: Semantics(
+                          label: 'Back button',
+                          child: TransparentButton(
+                            onPressed: () async {
+                              context.pop();
+                            },
+                            text: 'Go back',
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/transparent_button.dart
@@ -0,0 +1,62 @@
+import 'package:SwishLab/styles/colors.dart';
+import 'package:SwishLab/styles/styles.dart';
+import 'package:flutter/material.dart';
+
+class TransparentButton extends StatelessWidget {
+  final VoidCallback onPressed;
+  final String text;
+  final double height;
+  final EdgeInsetsGeometry? padding;
+  final EdgeInsetsGeometry iconPadding;
+  final Widget? icon;
+
+  const TransparentButton({
+    required this.onPressed,
+    required this.text,
+    this.height = 40,
+    this.padding = const EdgeInsets.symmetric(horizontal: 16),
+    this.iconPadding = EdgeInsets.zero,
+    this.icon,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final appColors = Theme.of(context).extension<AppColorSet>()!;
+
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        backgroundColor: appColors.transparentButtonBackground,
+        elevation: 0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8),
+          side: BorderSide(
+            color: appColors.transparentButtonBorders,
+            width: 2,
+          ),
+        ),
+        fixedSize: Size.fromHeight(height),
+        padding: padding,
+      ),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          if (icon != null)
+            Padding(
+              padding: iconPadding,
+              child: icon,
+            ),
+          if (icon != null) const SizedBox(width: 8),
+          Text(
+            text,
+            style: AppTextStyles.titleLarge(
+              context,
+              color: appColors.transparentButtonTextColor,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
